8fe284d9984e0eb4926e25b515e8777fc1ec1177
root <root@DESKTOP-QOASHEV.localdomain>
2025-12-15 14:56:33 +0800

    update

--- exercises/lifetimes/lifetimes2.rs
+++ exercises/lifetimes/lifetimes2.rs
@@ -2,11 +2,11 @@
 //
 // So if the compiler is just validating the references passed to the annotated
 // parameters and the return type, what do we need to change?
-//
+//那么如果编译器只是对传递给带标注参数和返回类型的引用进行有效性校验，我们需要修改什么呢？
 // Execute `rustlings hint lifetimes2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM  DONE
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
@@ -17,11 +17,8 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 }
 
 fn main() {
-    let string1 = String::from("long string is long");
-    let result;
-    {
-        let string2 = String::from("xyz");
-        result = longest(string1.as_str(), string2.as_str());
-    }
+    let string1 = String::from("abcd");
+    let string2 = "xyz";                
+    let result = longest(string1.as_str(), string2);
     println!("The longest string is '{}'", result);
 }
--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -13,18 +13,23 @@
 // Make the necessary code changes in the struct ReportCard and the impl block
 // to support alphabetical report cards. Change the Grade in the second test to
 // "A+" to show that your changes allow alphabetical grades.
+//// 一所虚构的魔法学校有一个用 Rust 编写的新版成绩单生成系统！
+// 目前该系统仅支持生成学生成绩为数字形式的成绩单（例如 1.0 -> 5.5）。
+// 但这所学校也会使用字母等级的成绩（A+ -> F-），因此需要能够打印这两种类型的成绩单！
 //
+// 请修改 `ReportCard` 结构体和 `impl` 代码块的必要部分，以支持字母等级的成绩单。
+// 将第二个测试中的成绩（Grade）改为 "A+"，以验证你的修改能够兼容字母等级的成绩。
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM  DONE
 
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: std::fmt::Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -52,7 +57,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
